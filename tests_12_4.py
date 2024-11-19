0ceedc65d42451a5cdda9b21706b59a348f84ecc
Orreyn <119194789+Orreyn@users.noreply.github.com>
2024-11-19 21:03:00 +0300

    Add files via upload

---
+++ tests_12_4.py
@@ -0,0 +1,99 @@
+import logging
+import unittest
+
+
+logging.basicConfig(level=logging.INFO, filemode='w', filename='runner_tests.log', format='%(levelname)s | %(message)s', encoding='UTF-8')
+
+
+class Runner:
+    def __init__(self, name, speed=5):
+        if isinstance(name, str):
+            self.name = name
+        else:
+            raise TypeError(f'Имя может быть только строкой, передано {type(name).__name__}')
+        self.distance = 0
+        if speed > 0:
+            self.speed = speed
+        else:
+            raise ValueError(f'Скорость не может быть отрицательной, сейчас {speed}')
+
+    def run(self):
+        self.distance += self.speed * 2
+
+    def walk(self):
+        self.distance += self.speed
+
+    def __str__(self):
+        return self.name
+
+    def __repr__(self):
+        return self.name
+
+    def __eq__(self, other):
+        if isinstance(other, str):
+            return self.name == other
+        elif isinstance(other, Runner):
+            return self.name == other.name
+
+
+class Tournament:
+    def __init__(self, distance, *participants):
+        self.full_distance = distance
+        self.participants = list(participants)
+
+    def start(self):
+        finishers = {}
+        place = 1
+        while self.participants:
+            for participant in self.participants:
+                participant.run()
+                if participant.distance >= self.full_distance:
+                    finishers[place] = participant
+                    place += 1
+                    self.participants.remove(participant)
+
+        return finishers
+
+
+class RunnerTest(unittest.TestCase):
+    is_frozen = False
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_walk(self):
+        try:
+            tw = Runner('Ходун', -5)
+            for i in range(10):
+                tw.walk()
+            self.assertEqual(tw.distance, 50)
+            logging.info(f'{self.test_walk().__name__} выполнен успешно')
+        except:
+            logging.warning('Неверная скорость для Runner', exc_info=True)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_run(self):
+        try:
+            tr = Runner(15)
+            for i in range(10):
+                tr.run()
+            self.assertEqual(tr.distance, 100)
+            logging.info(f'{self.test_run().__name__} выполнен успешно')
+        except:
+            logging.warning('Неверный тип данных для объекта Runner', exc_info=True)
+
+    @unittest.skipIf(is_frozen, 'Тесты в этом кейсе заморожены')
+    def test_challenge(self):
+        tw = Runner('Ходун')
+        tr = Runner('Бегун')
+        for i in range(10):
+            tw.walk()
+        for i in range(10):
+            tr.run()
+        self.assertNotEqual(tr.distance, tw.distance)
+
+
+first = Runner('Вося', 10)
+second = Runner('Илья', 5)
+third = Runner('Арсен', 10)
+
+t = Tournament(101, first, second)
+print(t.start())
